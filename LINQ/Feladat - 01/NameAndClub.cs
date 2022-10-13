bf66995792caf086500ae3716aa9224dad6ce500
Attila.Janovszki <b8janatt@vasvari.org>
2022-10-13 09:39:03 +0200

    10.13

---
+++ LINQ/Feladat - 01/NameAndClub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat___01
+{
+    public class NameAndClub
+    {
+        public string Name { get; set; }
+        public string Club { get; set; }
+
+        public NameAndClub()
+        {
+        }
+
+        public NameAndClub(string name, string club)
+        {
+            Name = name;
+            Club = club;
+        }
+    }
+}
--- LINQ/Feladat - 01/Program.cs
+++ LINQ/Feladat - 01/Program.cs
@@ -52,14 +52,13 @@ namespace Feladat___01
 
             //Milyen magas a legalacsonyabb játékos?
             double minHeight = _players.Min(player => player.Height);
-            Console.WriteLine(minHeight);
 
             //Milyen magas a legmagasabb játékos?
             double maxweight = _players.Max(player => player.Weight);
 
             //Van-e olyan játékos aki 1.78m magas?
             bool isAnyHeight178 = _players.Any(player => player.Height == 1.78);
-            Console.WriteLine(isAnyHeight178);
+
             //Kik játszanak a "Vasas óbuda" klubban?
             List<Player> vasasObudaPlayers = _players.Where(player => player.Club == "Vasas Óbuda")
                                                      .ToList();
@@ -74,7 +73,55 @@ namespace Feladat___01
                                                                       .OrderBy(player => player.Height) //OrderBy növekvő; OrderByDescending csökkenő
                                                                       .Select(player => player.Name)
                                                                       .ToList();
-            Console.ReadKey();
+
+            //Hány játékos játszik a vasas óbuda klubban?
+            //int vasasObudaPlayerscount = _players.Where(player => player.Club == "Vasas Óbuda")
+            //                                     .Count();
+            int vasasObudaPlayerscount = _players.Count(player => player.Club == "Vasas Óbuda");
+
+
+            //Rendezzük a játékosokat név szerint novekvo sorrendbe majd magasság szerint csokkenobe!
+            List<Player> orderedPlayers = _players.OrderBy(player => player.Name)
+                                                  .ThenByDescending(player => player.Height)
+                                                  .ToList();
+
+            //keressük ki azon jatekosokat akik libero poszton jatszanak és magasabbak mint 1.5 de és alacsonyabbak mint 1.71!
+            List<Player> liberos = _players.Where(player => player.Position == "liberó")
+                                           .Where(player => player.Height > 1.5)
+                                           .Where(player => player.Height < 1.71)
+                                           .ToList();
+            //Keresse ki a csapatok nevét!
+            List<string> clubs = _players.Select(player => player.Club)
+                                         .Distinct()
+                                         .ToList();
+
+            //keressük ki a jatekosok nevét és klubjat!
+            List<NameAndClub> playerNamesWithClubs = _players.Select(player => new NameAndClub(player.Name, player.Club))
+                                                             .ToList();
+
+            //List<NameAndClub> playerNamesWithClubs1 = _players.Select(player => new NameAndClub
+            //{
+            //    Name = player.Name,
+            //    Club = player.Club
+            //})
+            //.ToList();
+
+            //Keresse ki azon játékosok nevet akik uto(Hitter) poszton jatszanak, magasabbak mint 1.8 és a nevuk "a" beture vegzodik.
+            //Az eredmenyt rendezze csokkeno sorrendbe magassag szerint.
+
+            List<string> tallHitters = _players.Where(player => player.Position.ToLower() == "hitter")
+                                               .Where(player => player.Height > 1.8)
+                                               .Where(player => player.Name.EndsWith("a"))
+                                               .OrderByDescending(player => player.Height)
+                                               .Select(player => player.Name)
+                                               .ToList();
+
+            //Keresse ki a Juhar Dalma nevezetu jatekost. feltetelezheti, hogy csak egy jatekos van ezzel a nevvel!
+            //Player juharDalma1 = _players.SingleOrDefault(player => player.Name.ToLower() == "Juhár Dalma");
+            Player juharDalma = _players.FirstOrDefault(player => player.Name.ToLower() == "juhár dalma");
+
+            
+
         }
     }
 }
