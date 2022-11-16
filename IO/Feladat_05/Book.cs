49ab858e5f86c1546c4f3d97e9f73ea0d07f3a48
AttilaBLT <Attila.j.2003@gmail.com>
2022-11-16 18:52:26 +0100

    11.16 HF

--- IO/Feladat_05/Book.cs
+++ IO/Feladat_05/Book.cs
@@ -1,4 +1,5 @@
-﻿namespace Feladat_05;
+﻿namespace Feladat_05
+{
     public class Book
     {
         public string FirstName { get; set; }
@@ -7,35 +8,33 @@
         public string Title { get; set; }
         public string ISBN { get; set; }
         public string Releaser { get; set; }
-        public string ReleaseDate { get; set; }
+        public int ReleaseDate { get; set; }
         public int Price { get; set; }
         public string Theme { get; set; }
         public int PageNumber { get; set; }
         public int Honor { get; set; }
+        public Book()
+        {
 
-    public Book()
-    {
+        }
 
-    }
-
-    public Book(string firstName, string lastName, string birthDate, string title, string iSBN, string releaser, string releaseDate, int price, string theme, int pageNumber, int honor)
-    {
-        FirstName = firstName;
-        LastName = lastName;
-        BirthDate = birthDate;
-        Title = title;
-        ISBN = iSBN;
-        Releaser = releaser;
-        ReleaseDate = releaseDate;
-        Price = price;
-        Theme = theme;
-        PageNumber = pageNumber;
-        Honor = honor;
-    }
-
-    public override string ToString()
-    {
-        return $"{FirstName} {LastName} ({BirthDate}) - {Title} ({ISBN}) - {Releaser} ({ReleaseDate}) - {Price} - {Theme} - {PageNumber} - {Honor}";
+        public Book(string firstName, string lastName, string birthDate, string title, string iSBN, string releaser, int releaseDate, int price, string theme, int pageNumber, int honor)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            Title = title;
+            ISBN = iSBN;
+            Releaser = releaser;
+            ReleaseDate = releaseDate;
+            Price = price;
+            Theme = theme;
+            PageNumber = pageNumber;
+            Honor = honor;
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} - {BirthDate} - {Title} - {ISBN} - {Releaser} - {ReleaseDate} - {Price} - {Theme} - {PageNumber} - {Honor}";
+        }
     }
 }
-
--- IO/Feladat_05/Program.cs
+++ IO/Feladat_05/Program.cs
@@ -1,10 +1,33 @@
 ﻿List<Book> books = LoadData("./../../../_feladat_/adatok.txt");
 
+
+//1. - Írjuk ki a képernyőre az össz adatot
+
 PrintToScreen(books);
 
+//2.- Keressük ki az informatika témajú könyveket és mentsük el őket az informatika.txt állömányba
+
+List<Book> itBooks = books.Where(x => x.Theme.ToLower() == "informatika")
+                          .ToList();
+ITThematicBooks(itBooks);
+
+//3.- Az 1900.txt állományba mentsük el azokat a könyveket amelyek az 1900-as években íródtak
+
+List<Book> _1900books = books.Where(x => x.ReleaseDate >= 1900 && x.ReleaseDate <= 1999).ToList();
+Booksin1900s(_1900books);
+
+//4.- Rendezzük az adatokat a könyvek oldalainak száma szerint csökkenő sorrendbe
+//és a sorbarakott.txt állományba mentsük el.
+
+List<Book> orderByPageNumber = books.OrderByDescending(x => x.PageNumber).ToList();
+OrderByPageNumber(orderByPageNumber);
+
+//5.- „kategoriak.txt” állományba mentse el a könyveket téma szerint.
+
+List<string> themes = books.Select(x => x.Theme).Distinct().ToList();
+OrderByTheme(themes, books);
+
 Console.ReadKey();
-
-
 List<Book> LoadData(string filePath)
 {
     List<Book> bookData = new List<Book>();
@@ -22,7 +45,7 @@ List<Book> LoadData(string filePath)
             Title = data[3],
             ISBN = data[4],
             Releaser = data[5],
-            ReleaseDate = data[6],
+            ReleaseDate = int.Parse(data[6]),
             Price = int.Parse(data[7]),
             Theme = data[8],
             PageNumber = int.Parse(data[9]),
@@ -32,11 +55,66 @@ List<Book> LoadData(string filePath)
     }
     return bookData;
 }
-
 void PrintToScreen(ICollection<Book> booksdata)
 {
     foreach (Book book in booksdata)
     {
         Console.WriteLine(book);
+    }
+}
+
+void ITThematicBooks(ICollection<Book> books)
+{
+    string fileName = "./../../../_feladat_/informatika.txt";
+    using (FileStream fs = new FileStream(fileName, FileMode.Append, FileAccess.Write, FileShare.None))
+    {
+        using StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+        foreach (Book line in books)
+        {
+            sw.WriteLine(line.ToString());
+        }
+    }
+}
+void Booksin1900s(ICollection<Book> Booksin1900s)
+{
+    string filename = "./../../../_feladat_/1900.txt";
+    using (FileStream fs = new FileStream(filename, FileMode.Append, FileAccess.Write, FileShare.None))
+    {
+        using StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+        foreach (Book line in Booksin1900s)
+        {
+            sw.WriteLine(line.ToString());
+        }
+    }
+}
+void OrderByPageNumber(ICollection<Book> listorder)
+{
+    string fileName = "./../../../_feladat_/sorbarakott.txt";
+    using (FileStream fs = new FileStream(fileName, FileMode.Append, FileAccess.Write, FileShare.None))
+    {
+        using StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+        foreach (Book line in listorder)
+        {
+            sw.WriteLine(line.ToString());
+        }
+    }
+}
+void OrderByTheme(ICollection<string> themes, ICollection<Book> books)
+{
+    string fileName = "./../../../_feladat_/kategoriak.txt";
+    using (FileStream fs = new FileStream(fileName, FileMode.Append, FileAccess.Write, FileShare.None))
+    {
+        using StreamWriter sw = new StreamWriter(fs, Encoding.UTF8);
+        foreach (string theme in themes)
+        {
+            sw.WriteLine($"\t{theme}:");
+            foreach (Book book in books)
+            {
+                if (book.Theme == theme)
+                {
+                    sw.WriteLine(book.ToString());
+                }
+            }
+        }
     }
 }
